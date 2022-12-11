cfdf6269a9831091b622526d34a127d09ead428a
rizanb <anshu.aerim@gmail.com>
2022-12-11 17:18:28 +0545

    added state to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <div className='flex flex-wrap flex-col sm:flex-row bg-slate-800 items-center '>
-      <div className='basis-1/2 flex flex-wrap flex-row p-2 items-center'>
-        <div className=''>
-          <img className='w-8 h-8' src={logo} alt="" srcset="" />
+    const [showMenu, setShowMenu] = useState(false)
+    return (
+        <div className='mainNavigationBar flex flex-col bg-slate-800'>
+            <div className='p-4 flex flex-row'>
+                <div className='basis-1/2 text-white font-bold'>Finance Tracker</div>
+                <div className='basis-1/2 text-end sm:hidden'><a onClick={() => {setShowMenu(!showMenu)}} href='#'><i className="ri-menu-line"></i></a></div>
+                <div className='nav-links basis-1/2 justify-center hidden sm:block'>
+                    <ul className='flex flex-row justify-end text-gray-400'>
+                        <li><a className='p-4 hover:text-white' href="#">Home</a></li>
+                        <li><a className='p-4 hover:text-white' href="#">About</a></li>
+                        <li><a className='p-4 hover:text-white' href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className={showMenu == true? 'sm:hidden p-1 flex flex-col text-center bg-slate-700' : "hidden"} >
+                <ul>
+                    <li className='p-1 hover:font-bold hover:text-white'><a href="#">Home</a></li>
+                    <li className='p-1 hover:font-bold hover:text-white'><a href="#">About</a></li>
+                    <li className='p-1 hover:font-bold hover:text-white'><a href="#">Contact</a></li>
+
+                </ul>
+            </div>
         </div>
-        <div className='p-2 font-bold text-white'>
-          Finance Tracker
-        </div>
-      </div>
-      <div className='flex flex-col sm:flex-row basis-1/2 text-gray-400 text-center sm:text-start sm:justify-end  w-full sm:px-12'>
-       <a className='p-2 hover:text-white ' href="#">Home</a>
-       <a className='p-2 hover:text-white' href="#">Home</a>
-       <a className='p-2 hover:text-white' href="#">Home</a>
-       <a className='p-2 hover:text-white' href="#">Profile</a>
-      </div>
-    </div>
-  )
+
+    )
 }
 
 export default Navbar
+
